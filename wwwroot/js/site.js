44709459699f377539f7f879ab6a6822aa1b6a53
ejricahuerta <exricahuerta@gmail.com>
2019-10-28 22:38:12 -0400

    add test channels

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,46 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 var player;
-var songList = ["_BJo7YA9tno"]
+var songList = ["EAkIjq46Rlg"]
+var scoreList = []
+var randomPlaylist = []
+var randomPlayer;
+var channelList = ["UCIk6z4gxI5ADYK7HmNiJvNg", "UCbzz_Y9oVH2-57G4k0awE0w", "UCWtgXQ8Rc7H309esXN2gkrw", "UCUfLW7fYnY3A-5HCLgcK0_w", "UCaPwSXblS8F0owlKHGc6huw"]
+var list = {
+    playlist: ["'racks Planet Karaoke"]
+}
+
 function onYouTubePlayerAPIReady() {
-    player = new YT.Player('video', {
-      videoId: '0Bmhjf0rKe8',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
+    if (songList.length === 0) {
+        $("#player").addClass("d-none");
+        randomPlayer = setTimeout(function () {
+                $("#player").removeClass("d-none");
+                player = new YT.Player('player', {
+                    playerVars: {
+                        listType: 'playlist',
+                        list: randomPlaylist,
+                        autoplay: 1
+                    },
+                    events: {
+                        'onReady': onPlayerReady,
+                        'onStateChange': onPlayerStateChange
+                    }
+                });
+                $(".message").addClass("d-none").removeClass("d-flex");
+            },
+            1000)
+
+    } else {
+        clearTimeout(randomPlayer);
+        player = new YT.Player('player', {
+            videoId: songList[songList.length - 1],
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+        $(".message").addClass("d-none").removeClass("d-flex");
+    }
 }
 
 // autoplay video
@@ -18,16 +49,13 @@ function onPlayerReady(event) {
 }
 
 // when video ends
-function onPlayerStateChange(event) {        
-    if(event.data === 0) {            
-        if(songList === undefined || songList.length == 0) {
-            console.log("No songs available");
-            alert("song done..")
-        }
-        else{
-           var videoId = songList[songList.length-1];
-           player.loadVideoById(videoId)
+function onPlayerStateChange(event) {
+    if (event.data === 0) {
+        if (songList === undefined || songList.length == 0) {
+            $("#message").removeClass("d-none")
+        } else {
+            var videoId = songList[songList.length - 1];
+            player.loadVideoById(videoId)
         }
     }
 }
-
