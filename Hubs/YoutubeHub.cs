b4ddf934dead8bbbd77a64b94caccce8a19ecae4
ejricahuerta <exricahuerta@gmail.com>
2019-10-29 23:16:52 -0400

    clean up hub

--- Hubs/YoutubeHub.cs
+++ Hubs/YoutubeHub.cs
@@ -1,13 +1,14 @@
 using System.Threading.Tasks;
+using Karaoke.Entities;
+using Karaoke.Services;
 using Microsoft.AspNetCore.SignalR;
 
-namespace Karaoke.Hubs
-{
-    public class YoutubeHub : Hub
-    {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+namespace Karaoke.Hubs {
+    public class YoutubeHub : Hub {
+
+        public async Task SendSong (string user, string song) {
+            System.Console.WriteLine ("Fired!");
+            await Clients.All.SendAsync ("UpdateSong", user, song);
         }
     }
 }
