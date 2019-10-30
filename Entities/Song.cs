f763a48cfa601e721a554fabd49ecd262b2371ef
ejricahuerta <exricahuerta@gmail.com>
2019-10-29 23:16:43 -0400

    update song entity

--- Entities/Song.cs
+++ Entities/Song.cs
@@ -1,9 +1,12 @@
-namespace Karaoke.Entities
-{
-    public class Song
-    {
-        public string Id { get; set; }  
-        public string Title { get; set; }   
-        public string Artist { get; set; }
+using System.ComponentModel.DataAnnotations;
+
+namespace Karaoke.Entities {
+    public class Song {
+        [Key]
+        public int Id { get; set; }
+        public string SongId { get; set; }
+        public string Title { get; set; }
+        public string Channel { get; set; }
+        public string ChannelId { get; set; }
     }
 }
