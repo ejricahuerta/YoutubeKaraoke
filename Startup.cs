d8dd46418352734036c3e4b9dbc6095f7d32c304
ejricahuerta <exricahuerta@gmail.com>
2019-11-02 12:53:03 -0400

    updated database

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Karaoke.Entities;
 using Karaoke.Hubs;
@@ -18,15 +20,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Karaoke {
     public class Startup {
-        public Startup (IConfiguration configuration) {
-            Configuration = configuration;
-        }
 
+        public Startup (IConfiguration configuration) {
+            this.Configuration = configuration;
+
+        }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
-
 
             services.AddTransient<IYoutubeService, YoutubeService> ();
             services.AddTransient<IKaraokeService, KaraokeService> ();
@@ -66,16 +68,52 @@ namespace Karaoke {
                 routes.MapHub<YoutubeHub> ("/hub");
             });
             System.Console.WriteLine ($"SONG COUNT: {context.Songs.Count()}");
-            var songsToRemove = new List<Song>();
-            foreach (var song in context.Songs.Include(p=>p.SongId))
-            {
-                if(string.IsNullOrEmpty(song.SongId.VideoId.Trim())){
-                    songsToRemove.Add(song);
+            var songsToRemove = new List<Song> ();
+            var songsUpdated = new List<Song> ();
+            var listofWords = new List<string> {
+                "&quot;",
+                "(",
+                ")",
+                "Guitar",
+                "HD",
+                "Karaoke",
+                "lyrics",
+                "Instrumental",
+                "Version",
+                "Female key",
+                "Coversph",
+                "]",
+                "[",
+                "Acoustic",
+                "/",
+                "|",
+                "OST"
+
+            };
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex ("[ ]{2,}", options);
+            foreach (var song in context.Songs.Include (p => p.SongId).Include (p => p.Snippet)) {
+                if (string.IsNullOrEmpty (song.SongId.VideoId.Trim ())) {
+                    songsToRemove.Add (song);
                 }
+                var songTitle = song.Snippet.Title;
+                foreach (var item in listofWords) {
+                    songTitle = songTitle.Replace (item, string.Empty, true, CultureInfo.CurrentCulture);
+
+                    songTitle = regex.Replace (songTitle, " ");
+                    songTitle = songTitle.Replace ("-", " - ");
+                    songTitle = songTitle.Replace ("&amp;", "&");
+                    songTitle = songTitle.Replace ("&#39;", "'");
+
+                }
+                song.Snippet.Title = songTitle;
+                songsUpdated.Add (song);
+
             }
-            context.Songs.RemoveRange(songsToRemove);
-            context.SaveChanges();
-                        app.UseMvc ();
+            context.Songs.RemoveRange (songsToRemove);
+            context.Songs.UpdateRange (songsUpdated);
+            context.SaveChanges ();
+            app.UseMvc ();
         }
     }
 }
