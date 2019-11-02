0f215b719b08afb1070b77d2ba179a9f6c9a5972
Florentino Fernandez <boyet@Florentinos-MacBook-Pro.local>
2019-11-01 21:00:29 -0400

    updated chub connection

--- Hubs/YoutubeHub.cs
+++ Hubs/YoutubeHub.cs
@@ -10,9 +10,20 @@ namespace Karaoke.Hubs {
 
         }
         public async Task SendSong (string user, Song song) {
+            var songToPush = new SongSearched {
+                Title = song.Snippet.Title,
+                SongId = song.SongId.VideoId,
+            };
             System.Console.WriteLine ("Fired!");
-            await Clients.All.SendAsync ("UpdateSong", user, song);
+            await Clients.All.SendAsync ("UpdateSong", user, songToPush);
         }
 
+    }
+    class SongSearched
+    {
+        public int Id { get; set; }
+        public string SongId { get; set; }
+        public string Title { get; set; }
+    
     }
 }
